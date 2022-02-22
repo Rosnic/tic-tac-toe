1c5ad92ef462ff60ef9523af19ed7e6944853e81
Rosnic <100222749+Rosnic@users.noreply.github.com>
2022-02-22 19:51:18 +0100

    Upload of TicTacToe
    A TicTacToe with a GUI.

---
+++ MarkType.cs
@@ -0,0 +1,16 @@
+﻿namespace TicTacToe
+{
+
+    //The type of mark, a square can hold in the Tic Tac Toe game
+    internal enum MarkType
+    {
+        //The cell has not yet been marked
+        Free, 
+
+        //The cell has been marked with an "0"
+        Nought,
+
+        //The cell has been marked with an "X"
+        Cross
+    }
+}
